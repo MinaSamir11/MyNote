d02e0f4e84302f5320bc75bea6b5dc04789bfe2f
MinaSamir11 <menasamer11@gmail.com>
2020-07-19 07:04:25 +0200

    Android Project to save notes using room library and MVVM pattern

---
+++ app/src/main/java/com/example/sqlite/NoteDao.java
@@ -0,0 +1,29 @@
+package com.example.sqlite;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("Delete FROM note_table")
+    void deleteAll();
+
+    @Query("Select *FROM note_table order by priority desc")
+    LiveData<List<Note>> getAllNotes();
+
+}
